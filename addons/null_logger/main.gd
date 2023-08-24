a2da87d036f88bb93bcf72e55314bd0e68d2a1d3
NullStuff <88871218+NullStuff@users.noreply.github.com>
2023-08-24 09:43:07 -0500

    Fix project name and custom types.

--- addons/null_logger/main.gd
+++ addons/null_logger/main.gd
@@ -7,13 +7,16 @@ func _enter_tree():
 
 	add_custom_type("Logger", "Node", preload("./scripts/logger.gd"), preload("./assets/logger.svg"))
 	add_custom_type("FormattedLogger", "Logger", preload("./scripts/formatted_logger.gd"), preload("./assets/logger.svg"))
-	# add_custom_type("Transport", "Node", preload("./scripts/transport.gd"), preload("./assets/transport.svg"))
-	# add_custom_type("FileTransport", "Transport", preload("./scripts/file_transport.gd"), preload("./assets/transport.svg"))
-	# add_custom_type("ConsoleTransport", "Transport", preload("./scripts/console_transport.gd"), preload("./assets/transport.svg"))
+	add_custom_type("Transport", "Node", preload("./scripts/transport.gd"), preload("./assets/transport.svg"))
+	add_custom_type("FileTransport", "Transport", preload("./scripts/file_transport.gd"), preload("./assets/transport.svg"))
+	add_custom_type("ConsoleTransport", "Transport", preload("./scripts/console_transport.gd"), preload("./assets/transport.svg"))
 
 
 func _exit_tree():
 	remove_custom_type("Logger")
+	remove_custom_type("FormattedLogger")
 	remove_custom_type("Transport")
+	remove_custom_type("FileTransport")
+	remove_custom_type("ConsoleTransport")
 
 	remove_autoload_singleton("NullLogger")
